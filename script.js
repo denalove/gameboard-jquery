a661cd41240a83b84437d48b748b10e9b927dc01
Dena Sawyer <denasawyer1111@gmail.com>
2015-07-09 12:03:28 -0400

    Add toggle to elements and yellow to bell when clicked

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var main = function() {
 
+	$('.more-btn').on('click', function( event ) {
+		event.preventDefault();
+	  $(this).next().toggle();
+ });
+
+	$('.share').on('click', function() {
+	  $(this).next().toggle();
+ });
+
+	$('.notification').click(function() {
+	  $(this).toggleClass('active');
+  });
+
 };
 
 $(document).ready(main);
